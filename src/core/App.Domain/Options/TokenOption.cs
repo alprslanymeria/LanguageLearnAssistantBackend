db67e852405b4cee1c3ab79d33adfd513d4ccc3b
alparslan <alparslanorgnl@gmail.com>
2025-12-06 05:20:40 +0300

    options pattern implemented

---
+++ src/core/App.Domain/Options/TokenOption.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Domain.Options;
+
+public class TokenOption
+{
+    public const string Key = "TokenOptions";
+    public List<String> Audience { get; set; } = default!;
+    public string Issuer { get; set; } = default!;
+    public int AccessTokenExpiration { get; set; }
+    public string SecurityKey { get; set; } = default!;
+}
